bb3c1e9840b84790af5b0c2641a0f1f42a737f2f
FlutLab.io <>
2022-06-06 16:11:10 +0000

    Autosave

--- lib/Screen/dashboard.dart
+++ lib/Screen/dashboard.dart
@@ -13,42 +13,6 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Screen'),
       ),
-      body: Column(children: [
-        Container(
-          padding: EdgeInsets.all(8.0),
-        ),
-        Text(
-          'Nama: Aji Setiawan',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-        Text(
-          'NIM: 190103175',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-        Container(
-          padding: EdgeInsets.all(210.0),
-        ),
-        SizedBox(
-          width: 250,
-          height: 50,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-            onPressed: () {
-              var box = Hive.box('userBox').clear();
-
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => LoginScreen()),
-              );
-            },
-            child: Text('Logout'),
-          ),
-        ),
-      ]),
-    );
+      
   }
 }
