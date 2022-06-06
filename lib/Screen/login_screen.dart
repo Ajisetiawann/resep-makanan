8e060e010e121bacef99996b6c9ba11ed876aed9
FlutLab.io <>
2022-06-06 15:23:18 +0000

    Autosave

--- lib/Screen/login_screen.dart
+++ lib/Screen/login_screen.dart
@@ -21,7 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blueGrey,
       body: Column(
         children: [
           SizedBox(
