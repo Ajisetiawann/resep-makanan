104c94fb0097ed2e1ab2f3779db293d52465f4ee
FlutLab.io <>
2022-06-06 15:01:25 +0000

    Autosave

---
+++ lib/Screen/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:ajisetiawan/Screen/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:ajisetiawan/Screen/dashboard.dart';
+
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer() async {
+    var duration = Duration(seconds: 5);
+    return Timer(duration, () async {
+      var box = Hive.box('userBox');
+      bool? sudahLogin = box.get('isLogin');
+
+      if (sudahLogin ?? false) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => Dashboard()),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => LoginScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          '20220606_211017-removebg-preview.png',
+        ),
+        CircularProgressIndicator(),
+      ],
+    );
+  }
+}
