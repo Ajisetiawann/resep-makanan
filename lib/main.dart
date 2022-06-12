026f9e9745cc0a46da71af537ce931c45856b1c4
FlutLab.io <>
2022-06-12 06:19:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,24 @@
-import 'package:ajisetiawan/Screen/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:ajisetiawan/Screen/splash_screen.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:resep_makanan/views/home.dart';
 
-void main() async {
-  await Hive.initFlutter();
-  var box = await Hive.openBox('userBox');
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Resep Makanan',
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
       ),
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
