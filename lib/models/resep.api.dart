ccc295d75f24b99ce12bbaa6082b6ef7ae63fc7e
FlutLab.io <>
2022-06-12 06:39:53 +0000

    Autosave

---
+++ lib/models/resep.api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:resep_makanan/models/resep.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Resep>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list', {
+      "limit": "24",
+      "start": "0"
+    });
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "f0b8b0f930mshca4fb6b3fa97cb7p1cf020jsnef3341bb1d16",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Resep.recipesFromSnapshot(_temp);
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:resep_makanan/models/resep.api.dart';
+import 'package:resep_makanan/models/resep.dart';
 import 'package:resep_makanan/views/widgets/resep_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,6 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Resep> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +34,17 @@ class _HomePageState extends State<HomePage> {
             children: [
               Icon(Icons.restaurant_menu),
               SizedBox(width: 10),
-              Text('Resep Makanan'),
+              Text('Food Recipe')
             ],
           ),
         ),
-        body: ResepCard(
-          title: 'My recipe',
-          rating: '4.9',
-          cookTime: '30 min',
-          thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
-        ));
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _recipes.length,
+                itemBuilder: (context, index) {
+                  return ResepCard(title: _recipes[index].name, cookTime: _recipes[index].totalTime, rating: _recipes[index].rating.toString(), thumbnailUrl: _recipes[index].images);
+                },
+              ));
   }
 }
