6fe66a231f7a9c5a165ef14703c1b57df3666f79
FlutLab.io <>
2022-06-12 06:21:45 +0000

    Autosave

--- lib/views/widgets/resep_card.dart
+++ lib/views/widgets/resep_card.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class RecipeCard extends StatelessWidget {
+class ResepCard extends StatelessWidget {
   final String title;
   final String rating;
   final String cookTime;
   final String thumbnailUrl;
-  RecipeCard({
+  ResepCard({
     @required this.title,
     @required this.cookTime,
     @required this.rating,
