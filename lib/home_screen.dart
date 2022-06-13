6f6f2875a33954c6dca5956e784478762faefa2e
FlutLab.io <>
2022-06-13 16:46:33 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,7 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
             backgroundColor: Colors.redAccent[700],
             title: Row(
               children: [
-                Image.asset('images/gambar1.png', height: 120),
+                Image.asset('images/gambar2.png', height: 120),
               ],
             )),
         body: Stack(children: <Widget>[
