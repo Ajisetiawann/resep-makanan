6e38a57bf0e6276296f6f9c890692c1a9200d3b5
FlutLab.io <>
2022-06-19 13:22:06 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 image: DecorationImage(
                     alignment: Alignment.topCenter,
                     image: AssetImage(
-                      'images/1.png',
+                      'images/gambarr.png',
                     ))),
           ),
           SafeArea(
