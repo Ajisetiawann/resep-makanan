05f8271092f4e57f51908a00b4763516aa4720eb
FlutLab.io <>
2022-06-19 11:11:27 +0000

    Autosave

--- lib/views/detail_video.dart
+++ lib/views/detail_video.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class DetailVideo extends StatefulWidget {
   final String videoUrl;
@@ -13,25 +15,45 @@ class _DetailVideoState extends State<DetailVideo> {
   final String videoUrl;
   _DetailVideoState(this.videoUrl);
 
+  late VideoPlayerController playerController;
+
+  @override
+  void initState() {
+    super.initState();
+    playerController = VideoPlayerController.network(videoUrl)
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((value) => playerController.play());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    playerController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.redAccent[700],
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.restaurant_menu),
-              SizedBox(width: 10),
-              Text('Resep Video'),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent[700],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(width: 10),
+            Text('Resep Video'),
+          ],
         ),
-        body: Center(
-          child: Text(
-            videoUrl,
-            style: TextStyle(color: Colors.black),
-          ),
-        ));
+      ),
+      body: Center(
+        child: playerController.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: playerController.value.aspectRatio,
+                child: VideoPlayer(playerController),
+              )
+            : Container(),
+      ),
+    );
   }
 }
