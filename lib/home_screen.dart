19936ee097fe0a7b3c8e7f00cea90ffbc19b8d80
FlutLab.io <>
2022-06-30 16:25:52 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:resep_makanan/screens/components/body.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,18 +12,78 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: buildAppBar(),
-      body: Body(),
-    );
-  }
-
-  AppBar buildAppBar() {
-    return AppBar(
-      elevation: 0,
-      leading: IconButton(
-        icon: SvgPicture.asset("images/menu.svg"),
-        onPressed: () {},
-      ),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.redAccent[700],
+            title: Row(
+              children: [
+                Image.asset('images/gambar1.png', height: 120),
+              ],
+            )),
+        body: Stack(children: <Widget>[
+          Container(
+            height: size.height * .4,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    alignment: Alignment.topCenter,
+                    image: AssetImage(
+                      'images/Group1.png',
+                    ))),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      height: 64,
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Mau Masak Apa', style: TextStyle(color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold)),
+                              Text('Hari Ini?', style: TextStyle(color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold)),
+                            ],
+                          )
+                        ],
+                      )),
+                  Expanded(
+                    child: GridView.count(
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        primary: false,
+                        children: <Widget>[
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          ),
+                        ],
+                        crossAxisCount: 2),
+                  )
+                ],
+              ),
+            ),
+          )
+        ]));
   }
 }
