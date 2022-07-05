fcdf8d4988300cf4bf77e90dd1b861684a6b45a8
FlutLab.io <>
2022-07-05 14:52:45 +0000

    Autosave

--- lib/screens/details/components/image_dan_icons.dart
+++ lib/screens/details/components/image_dan_icons.dart
@@ -5,12 +5,10 @@ import '../../../constants.dart';
 import 'icon_card.dart';
 
 class ImageAndIcons extends StatelessWidget {
-  const ImageAndIcons({
-    Key? key,
-    required this.size,
-  }) : super(key: key);
-
   final Size size;
+  const ImageAndIcons({
+    required this.size,
+  });
 
   @override
   Widget build(BuildContext context) {
