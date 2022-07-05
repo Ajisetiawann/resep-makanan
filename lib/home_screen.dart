556253e8d4d1adb25896a41f8c76423ec35e17ca
FlutLab.io <>
2022-07-05 14:17:17 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:resep_makanan/screens/components/body.dart';
+import 'package:resep_makanan/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
