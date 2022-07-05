5d7557952912f81d8f660198f64a59682d47b462
FlutLab.io <>
2022-07-05 16:14:28 +0000

    Autosave

--- lib/screens/home/components/title_with_more_bbtn.dart
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -27,7 +27,7 @@ class TitleWithMoreBtn extends StatelessWidget {
             onPressed: press,
             child: Text(
               "More",
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Colors.black),
             ),
           ),
         ],
