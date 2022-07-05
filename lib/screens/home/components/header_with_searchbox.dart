bcf8df4a9f96e52b178928222735a8d670961885
FlutLab.io <>
2022-07-05 15:14:20 +0000

    Autosave

--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -36,7 +36,7 @@ class HeaderWithSearchBox extends StatelessWidget {
             child: Row(
               children: <Widget>[
                 Text(
-                  'Mau Masak apa hari ini?',
+                  'Hello',
                   style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
                 ),
                 Spacer(),
