d6b6aa351a41d2aef5a79cec8b59f9f51df48be9
FlutLab.io <>
2022-07-07 14:11:00 +0000

    Autosave

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -12,7 +12,7 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ImageAndIcons(size: size),
-          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          TitleAndPrice(title: "Korean Corn Dogs", country: "Korea", price: 440),
           SizedBox(height: kDefaultPadding),
           Row(
             children: <Widget>[
