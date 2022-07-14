912d8b2feed1bdaf10da8082a2c3c2a6f5cb5c89
FlutLab.io <>
2022-07-14 14:38:47 +0000

    Autosave

--- lib/screens/home/components/kategori.dart
+++ lib/screens/home/components/kategori.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resep_makanan/screens/details/components/detail_kategori.dart';
+import 'package:resep_makanan/screens/details/components/detail_screen.dart';
 
 import '../../../constants.dart';
 
@@ -22,7 +23,7 @@ class Kategori extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => DetailKategori(),
+                  builder: (context) => DetailsScreen(),
                 ),
               );
             },
@@ -35,7 +36,7 @@ class Kategori extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => DetailKategori(),
+                  builder: (context) => DetailsScreen(),
                 ),
               );
             },
