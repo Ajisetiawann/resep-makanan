fbc94a431ea4dfcf8ca15c309394fb796df8deb7
FlutLab.io <>
2022-07-14 16:34:03 +0000

    Autosave

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -12,7 +12,7 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ImageAndIcons(size: size),
-          TitleAndPrice(title: "Korean Corn Dogs ", country: "Korea"),
+          TitleAndPrice(title: "Korean ", country: "Corn Dogs"),
           SizedBox(height: kDefaultPadding),
           Row(
             children: <Widget>[
---
+++ lib/screens/details/components/detail_espresso.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:resep_makanan/constants.dart';
+
+import 'image_dan_icons.dart';
+import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Espresso ", country: "Martini"),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text("Description"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
