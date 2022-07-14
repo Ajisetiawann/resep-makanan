d48317ca54914cfdd7ba3c2b01a8dff6989d18b0
FlutLab.io <>
2022-07-14 13:41:53 +0000

    Autosave

--- lib/kategori.dart
+++ lib/kategori.dart
@@ -2,12 +2,13 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:resep_makanan/screens/home/components/body.dart';
+import 'package:resep_makanan/screens/home/components/menukategori.dart';
 
-class HomeScreen extends StatelessWidget {
+class KategoriMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MyHomePage(),
+      body: MenuKategori(),
     );
   }
 }
