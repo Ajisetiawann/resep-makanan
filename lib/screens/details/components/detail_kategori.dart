11ae4cebd5e54fb636b174aa655ebde6be7b5ed8
FlutLab.io <>
2022-07-14 14:43:48 +0000

    Autosave

--- lib/screens/details/components/detail_kategori.dart
+++ lib/screens/details/components/detail_kategori.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:resep_makanan/screens/details/components/resepdetail.dart';
 import 'package:resep_makanan/screens/home/components/menukategori.dart';
 
 class DetailKategori extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MenuKategori(),
+      body: RecipeDetailsetails(),
     );
   }
 }
